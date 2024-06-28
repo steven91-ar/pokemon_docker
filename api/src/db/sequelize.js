d440c2120287335a7cc5ec24c597f4c0daadc981
steven9work <72232262+steven91-ar@users.noreply.github.com>
2024-06-28 16:37:54 +0200

    pokemon_docker

---
+++ api/src/db/sequelize.js
@@ -0,0 +1,58 @@
+const { Sequelize, DataTypes } = require('sequelize')
+const bcrypt = require('bcrypt')
+const PokemonModel = require('../models/pokemon')
+const UserModel = require('../models/user')
+const pokemons = require('./mock-pokemon')
+
+let sequelize
+
+if(process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize('pokemon', 'root', '', {
+    host: 'localhost',
+    port: '3307',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: true
+  })
+} else {
+  sequelize = new Sequelize('pokemon', 'root', '', {
+    host: 'localhost',
+    dialect: 'mariadb',
+    port: '3307',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: true
+  })
+  
+}
+
+const Pokemon = PokemonModel(sequelize, DataTypes)
+const User = UserModel(sequelize, DataTypes)
+
+const initDb = () => {
+  return sequelize.sync({ force: true, alter: true }).then(_ => {
+    pokemons.map(pokemon => {
+      Pokemon.create({
+        name: pokemon.name,
+        hp: pokemon.hp,
+        cp: pokemon.cp,
+        picture: pokemon.picture,
+        types: pokemon.types
+      })
+      .then(pokemon => console.log(pokemon.toJSON()))
+    })
+
+    bcrypt.hash('pikachu', 10)
+    .then(hash => User.create({ username: 'pikachu', password: hash }))
+    .then(user => console.log(user.toJSON()))
+
+    console.log('La base de donnée a bien été initialisée !')
+  })
+}
+
+module.exports = { 
+  initDb, Pokemon, User
+}
